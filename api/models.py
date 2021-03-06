7a20cea91eb526139a8d8be2cf23b7683fb269d4
nateselzer <nselzer@butterflynetinc.com>
2021-03-06 15:27:06 -0500

    feat: add transaction model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django_enumfiled import enum
+from enum import Enum
 
 # Create your models here.
+
+class Status(Enum):
+    STARTED = 0
+    IN_PROGRESS = 1
+    FAILED = 2
+    CANCELLED = 3
+    SUCCEEDED = 4
+
+ 
+class Transaction(models.Model):
+    video_url = models.UrlField(max_length=120)
+    status = enum.EnumField(Status, default=Status.STARTED)
